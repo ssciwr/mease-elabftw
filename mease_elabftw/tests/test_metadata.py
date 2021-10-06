8901c2d11cf5f742b3d65375c4d33c0368adf9bb
Liam Keegan <liam@keegan.ch>
2021-10-06 17:42:46 +0200

    add tests for missing/invalid ELABFTW_TOKEN

--- mease_elabftw/tests/test_metadata.py
+++ mease_elabftw/tests/test_metadata.py
@@ -9,7 +9,29 @@ def test_get_valid_id():
     assert len(data["Electrophysiology"].keys()) == 1
 
 
-def test_get_invalid_id():
+def test_get_no_token(monkeypatch):
+    monkeypatch.delenv("ELABFTW_TOKEN")
+    with pytest.raises(Exception) as exception_info:
+        data = elm.get(1)
+    assert exception_info.type == RuntimeError
+    assert (
+        str(exception_info.value)
+        == "The ELABFTW_TOKEN environment variable needs to be set to your eLabFTW access token."
+    )
+
+
+def test_get_invalid_token(monkeypatch):
+    monkeypatch.setenv("ELABFTW_TOKEN", "abc123")
+    with pytest.raises(Exception) as exception_info:
+        data = elm.get(1)
+    assert exception_info.type == RuntimeError
+    assert (
+        str(exception_info.value)
+        == "Could not connect to https://elabftw.uni-heidelberg.de - do you have a valid token?"
+    )
+
+
+def test_get_invalid_id(monkeypatch):
     with pytest.raises(Exception) as exception_info:
         data = elm.get(1)
     assert exception_info.type == RuntimeError
