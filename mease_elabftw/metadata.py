ff7a0f1189dcf255172757b40b46407219f532d0
Liam Keegan <liam@keegan.ch>
2021-10-06 17:12:22 +0200

    add some error handling

--- mease_elabftw/metadata.py
+++ mease_elabftw/metadata.py
@@ -5,9 +5,29 @@ from requests.exceptions import HTTPError
 
 
 def get(experiment_id):
+    url = "https://elabftw.uni-heidelberg.de"
     token = os.environ.get("ELABFTW_TOKEN")
-    manager = elabapy.Manager(
-        endpoint="https://elabftw.uni-heidelberg.de/api/v1/", token=token
-    )
-    experiment = manager.get_experiment(experiment_id)
-    return json.loads(experiment["metadata"])
+    if token is None:
+        raise RuntimeError(
+            "The ELABFTW_TOKEN environment variable needs to be set to your eLabFTW access token."
+        )
+    manager = elabapy.Manager(endpoint=url + "/api/v1/", token=token)
+    try:
+        experiment = manager.get_experiment(experiment_id)
+    except HTTPError as e:
+        if e.response.status_code == 400:
+            raise RuntimeError(
+                f"Could not connect to {url} - do you have a valid token?"
+            )
+        elif e.response.status_code == 403:
+            raise RuntimeError(
+                f"Experiment with id {experiment_id} not found - do you have the correct id?"
+            )
+        else:
+            raise e
+    metadata = experiment["metadata"]
+    if metadata is None:
+        raise RuntimeError(
+            f"Experiment with id {experiment_id} doesn't contain any metadata."
+        )
+    return json.loads(metadata)
--- mease_elabftw/tests/test_metadata.py
+++ mease_elabftw/tests/test_metadata.py
@@ -1,8 +1,29 @@
+import pytest
 import mease_elabftw.metadata as elm
 
 
-def test_get():
+def test_get_valid_id():
     data = elm.get(156)
     assert len(data.keys()) == 2
     assert len(data["Custom"].keys()) == 6
     assert len(data["Electrophysiology"].keys()) == 1
+
+
+def test_get_invalid_id():
+    with pytest.raises(Exception) as exception_info:
+        data = elm.get(1)
+    assert exception_info.type == RuntimeError
+    assert (
+        str(exception_info.value)
+        == "Experiment with id 1 not found - do you have the correct id?"
+    )
+
+
+def test_get_valid_id_no_metadata():
+    with pytest.raises(Exception) as exception_info:
+        data = elm.get(145)
+    assert exception_info.type == RuntimeError
+    assert (
+        str(exception_info.value)
+        == "Experiment with id 145 doesn't contain any metadata."
+    )
