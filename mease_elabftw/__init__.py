e3feca7a730c0bd9a7af2a3b29eeecd88490b689
Liam Keegan <liam@keegan.ch>
2021-10-28 13:57:51 +0200

    add `get_linked_items` to retrieve and parse all linked database items for an experiment
    - parses HTML body assuming key/value pairs are separated by a colon, and each pair is contained inside a html tag, e.g. <p>

--- mease_elabftw/__init__.py
+++ mease_elabftw/__init__.py
@@ -1,2 +1,3 @@
 from .metadata import get_metadata
+from .linked_items import get_linked_items
 from .experiments import list_experiments, upload_file
---
+++ mease_elabftw/tests/test_linked_items.py
@@ -0,0 +1,60 @@
+import pytest
+import mease_elabftw
+import test_ids
+
+
+def test_get_linked_items():
+    items = mease_elabftw.get_linked_items(test_ids.valid_experiment)
+    assert len(items) == 3
+    assert items[0]["category"] == "dye"
+    assert items[0]["title"] == "DiO green"
+    d = items[0]["data_dict"]
+    assert len(d.items()) == 2
+    assert d["origin"] == "ThermoFisher"
+    assert d["catalog number"] == "V22886"
+    assert items[1]["category"] == "mouse line"
+    assert items[1]["title"] == "wild type"
+    d = items[1]["data_dict"]
+    assert len(d.items()) == 6
+    assert items[2]["category"] == "virus"
+    assert items[2]["title"] == "AAVretr Flpo"
+    d = items[2]["data_dict"]
+    assert len(d.items()) == 5
+    assert d["Virus in -80 storage"] == "AAVretr EF1a-Flpo"
+
+
+def test_get_linked_items_no_token(monkeypatch):
+    monkeypatch.delenv("ELABFTW_TOKEN")
+    with pytest.raises(Exception) as exception_info:
+        items = mease_elabftw.get_linked_items(test_ids.valid_experiment)
+    assert exception_info.type == RuntimeError
+    assert (
+        str(exception_info.value)
+        == "The ELABFTW_TOKEN environment variable needs to be set to your eLabFTW access token."
+    )
+
+
+def test_get_linked_items_invalid_token(monkeypatch):
+    monkeypatch.setenv("ELABFTW_TOKEN", "abc123")
+    with pytest.raises(Exception) as exception_info:
+        items = mease_elabftw.get_linked_items(test_ids.valid_experiment)
+    assert exception_info.type == RuntimeError
+    assert (
+        str(exception_info.value)
+        == "Could not connect to https://elabftw.uni-heidelberg.de - do you have a valid token?"
+    )
+
+
+def test_get_linked_items_invalid_id(monkeypatch):
+    with pytest.raises(Exception) as exception_info:
+        items = mease_elabftw.get_linked_items(test_ids.invalid_experiment)
+    assert exception_info.type == RuntimeError
+    assert (
+        str(exception_info.value)
+        == f"Experiment with id {test_ids.invalid_experiment} not found - do you have the correct id?"
+    )
+
+
+def test_get_linked_items_no_items():
+    items = mease_elabftw.get_linked_items(test_ids.valid_experiment_no_items)
+    assert len(items) == 0
