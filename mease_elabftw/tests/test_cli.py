c414711285eb190708eb207dc9fabaa0aa46f001
Liam Keegan <liam@keegan.ch>
2022-05-10 15:14:31 +0200

    add example notebook
    - list experiments by owner
    - get experiments by owner, excluding those with an existing upload that matches supplied filename
    - display dict containing all info from experiment
    - upload a file to an experiment
    
    also add an experiment to elabftw to use here, update tests accordingly

--- mease_elabftw/tests/test_cli.py
+++ mease_elabftw/tests/test_cli.py
@@ -7,10 +7,11 @@ def test_elabftw_list():
     result = runner.invoke(elabftw_list, args="Liam")
     assert result.exit_code == 0
     lines = result.output.split("\n")
-    assert len(lines) == 3
-    assert lines[0][:4] == "163:"
-    assert lines[1][:4] == "156:"
-    assert lines[2] == ""
+    assert len(lines) == 4
+    assert lines[0][:5] == "1128:"
+    assert lines[1][:4] == "163:"
+    assert lines[2][:4] == "156:"
+    assert lines[3] == ""
 
 
 def test_elabftw_list_no_token(monkeypatch):
